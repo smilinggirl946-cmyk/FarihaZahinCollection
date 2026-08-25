a63b9ff7760b2435dfd084738fdaed1098477070
Fariha Zahin <smilinggirl946@gmail.com>
2026-08-25 18:05:14 +0600

    Create main.js

---
+++ BattleIsland/main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    minWidth: 900,
+    minHeight: 600,
+    autoHideMenuBar: true,
+    backgroundColor: "#050912",
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false
+    }
+  });
+
+  win.loadFile(path.join(__dirname, "BattleIsland.html"));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
